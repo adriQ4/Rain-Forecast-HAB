f047709afa22381407857c528c436b200e8a06a2
Isaquinsan12 <155665641+Isaquinsan12@users.noreply.github.com>
2024-03-18 14:11:57 +0100

    actualizado

--- app.js
+++ app.js
@@ -1,55 +1,57 @@
 window.onload = (event) => {
-    setTimeout(function() {
-        const loader = document.getElementById('loader');
-        loader.style.display = 'none';
-    }, 1000);
-}
-function loader(){
-    const loader = document.getElementById('loader');
-        loader.style.display = 'flex';
-        setTimeout(function() {;
-            loader.style.display = 'none';
-        }, 5000);
+loader(1500);
+};
 
-}
-//
+// Ejecucion de Botón
 
-const btn = document.getElementById('boton-principal');
-
-btn.addEventListener('click', function() {
-    loader()
-    position()
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            let latitud = position.coords.latitude;
-            let longitud = position.coords.longitude;
-        }, function(error) {
-            console.log('Error: ' + error.message);
-        }, {
-            maximumAge: 60000
-        })
-    } else {
-        console.log('La Geolocalización no es soportada por este navegador.');
+const btn = document.getElementById("boton-principal");
+btn.addEventListener("click", function () {
+loader(5000);
+position();
+redirigir();
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+    function (position) {
+        let latitud = position.coords.latitude;
+        let longitud = position.coords.longitude;
+    },
+    function (error) {
+        console.log(`Error: ${error.message}`);
+    },
+    {
+        maximumAge: 60000,
     }
+    );
+} else {
+    console.log("La Geolocalización no es soportada por este navegador.");
+}
 });
 
-function position(){
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            let lat = position.coords.latitude;
-            let lon = position.coords.longitude;
-            let url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=rain&forecast_days=1`
-            fetch(url)
-        .then(data => data.json())
-        .then(response => console.log(response));;
-        });
-    } else { 
-        console.log("La Geolocalización no es soportada por este navegador.");
-    }
-
+// *FUNCION PARA LOADER
+function loader(miliseconds) {
+const loader = document.getElementById("loader");
+loader.style.display = "flex";
+setTimeout(function () {
+    loader.style.display = "none";
+}, miliseconds);
 }
 
-
-
-
-
+function position() {
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=rain&forecast_days=1`;
+    fetch(url)
+        .then((data) => data.json())
+        .then((response) => console.log(response))
+        .catch((error) => console.log(`Error Fetch: ${error.message}`));
+    });
+} else {
+    console.log("La Geolocalización no es soportada por este navegador.");
+}
+}
+// ! REDIRIGIR A LA PAGINA DEL RESULTADO
+function redirigir() {
+window.location.href = "index2.html";
+}
