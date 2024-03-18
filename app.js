2563af90edac1460930285b8585434794157364a
adriq4 <adri230992@gmail.com>
2024-03-18 12:52:13 +0100

    actualizacion

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
-
-setTimeout(function (e) {
+window.onload = (event) => {
+    setTimeout(function() {
+        const loader = document.getElementById('loader');
+        loader.style.display = 'none';
+    }, 1000);
+}
+function loader(){
     const loader = document.getElementById('loader');
-    loader.style.display = 'none';
-},5000);
+        loader.style.display = 'flex';
+        setTimeout(function() {;
+            loader.style.display = 'none';
+        }, 5000);
+
+}
+//
+
+const btn = document.getElementById('boton-principal');
+
+btn.addEventListener('click', function() {
+    loader()
+    position()
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let latitud = position.coords.latitude;
+            let longitud = position.coords.longitude;
+        }, function(error) {
+            console.log('Error: ' + error.message);
+        }, {
+            maximumAge: 60000
+        })
+    } else {
+        console.log('La Geolocalización no es soportada por este navegador.');
+    }
+});
+
+function position(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+            let url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=rain&forecast_days=1`
+            fetch(url)
+        .then(data => data.json())
+        .then(response => console.log(response));;
+        });
+    } else { 
+        console.log("La Geolocalización no es soportada por este navegador.");
+    }
+
+}
 
 
 
